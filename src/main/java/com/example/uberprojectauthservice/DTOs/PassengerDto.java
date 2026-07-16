bd23443f035cd97a6ced90ac59272043c3fbc37a
Rishabh <10septrb@gmail.com>
2026-07-16 14:30:11 +0530

    Added first plain API with DTOs

---
+++ src/main/java/com/example/uberprojectauthservice/DTOs/PassengerDto.java
@@ -0,0 +1,36 @@
+package com.example.uberprojectauthservice.DTOs;
+
+import lombok.*;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PassengerDto {
+
+    private String id;
+
+    private String name;
+
+    private String email;
+
+    private String password; // encrypted password
+
+    private String phoneNumber;
+
+    private Date createdAt;
+
+    public static PassengerDto from(Passenger p) {
+        return PassengerDto.builder()
+                .id(p.getId().toString())
+                .createdAt(p.getCreatedAt())
+                .email(p.getEmail())
+                .password(p.getPassword())
+                .phoneNumber(p.getPhoneNumber())
+                .name(p.getName())
+                .build();
+    }
+}
---
+++ src/main/java/com/example/uberprojectauthservice/DTOs/PassengerSignupRequestDto.java
@@ -0,0 +1,19 @@
+package com.example.uberprojectauthservice.DTOs;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PassengerSignupRequestDto {
+
+    private String email;
+
+    private String password;
+
+    private String phoneNumber;
+
+    private String name;
+}
