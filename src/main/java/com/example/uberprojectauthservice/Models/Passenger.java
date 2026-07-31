3131fb3f96cd3c6c0bfd7203ec131ab7d3e3dfd1
Rishabh <10septrb@gmail.com>
2026-07-31 16:35:10 +0530

    Added password hashing using Bcrypt

--- src/main/java/com/example/uberprojectauthservice/Models/Passenger.java
+++ src/main/java/com/example/uberprojectauthservice/Models/Passenger.java
@@ -4,15 +4,13 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Builder
 @Getter
 @Setter
 @NoArgsConstructor
