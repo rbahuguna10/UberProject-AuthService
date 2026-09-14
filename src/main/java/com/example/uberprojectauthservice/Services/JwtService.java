e8f4a4348db36fe71121a80da58669670cec9285
Rishabh <10septrb@gmail.com>
2026-09-14 17:20:48 +0530

    JWT creation service & added Payload extraction methods

---
+++ src/main/java/com/example/uberprojectauthservice/Services/JwtService.java
@@ -0,0 +1,96 @@
+package com.example.uberprojectauthservice.Services;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
+public class JwtService implements CommandLineRunner {
+
+    @Value("${jwt.expiry}")
+    private int expiry;
+
+    @Value("${jwt.secret}")
+    private String SECRET;
+
+    /**
+     * This method creates a brand-new JWT token for us based on a payload
+     * @return
+     */
+    private String createToken(Map<String, Object> payload, String email) {
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + expiry*1000L);
+        return Jwts.builder()
+                .claims(payload) // .claims() -> function to set the payloads
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(expiryDate)
+                .subject(email)
+                .signWith(getSignKey())
+                .compact(); // Builds the JWT and serializes it to a compact, URL-safe string
+    }
+
+    private Claims extractAllPayloads(String token) {
+        return Jwts
+                .parser()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllPayloads(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration); // ::getExpiration -> from "createToken"
+    }
+
+    private String extractEmail(String token) {
+        return extractClaim(token, Claims::getSubject); // ::getSubject -> from "createToken"
+    }
+
+    /**
+     * This method checks if the token expiry was before the current time stamp or not ?
+     * @param token JWT token
+     * @return true if token is expired else false
+     */
+    private Boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Key getSignKey() {
+        return Keys.hmacShaKeyFor(SECRET.getBytes(StandardCharsets.UTF_8));
+    }
+
+    private Boolean validateToken(String token, String email) {
+        final String userEmailFetchedFromToken = extractEmail(token);
+        return (userEmailFetchedFromToken.equals(email)) && !isTokenExpired(token);
+    }
+
+    private Object extractPayload(String token, String payloadKey) {
+        Claims claim = extractAllPayloads(token);
+        return (Object) claim.get(payloadKey);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Map<String, Object> mp = new HashMap<>();
+        mp.put("email", "a@b.com");
+        mp.put("phoneNumber", "9999999999");
+        String result = createToken(mp, "Rishabh");
+        System.out.println("Generated token is: " + result);
+        System.out.println(extractPayload(result, "email").toString());
+    }
+}
